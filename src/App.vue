34a0503e3825292c0d1c424951ccb7c62a3a7790
Alasdair Monk <alasdair.monk@gmail.com>
2016-03-10 17:43:15 +0000

    Add hashtags

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,23 @@
     <weather v-if="hasData" :weather="weather" :location="location" :tweets="tweets" :reloading="!hasData"
     ></weather>
     <div class="flex flex-wrap m1">
-      <div v-for="tweet in tweets" class="flex-none m1">
+      <div v-for="tweet in tweets" class="flex m1">
         <img height="46" class="rounded avatar" v-bind:src="tweet.user.profile_image_url_https"/>
       </div>
-    <div>
+    </div>
+
+    <hr class="my0"/>
+
+    <div class="p1">
+      <template v-for="tweet in tweets">
+        <span v-for="hashtag in tweet.entities.hashtags">
+          <a target="_blank" href="https://twitter.com/search?q={{hashtag.text}}" class="hashtag">#{{hashtag.text}}</a>
+        </span>
+      </template>
+    </div>
   </div>
+
+
 </template>
 
 <script>
@@ -61,6 +73,19 @@ export default {
 </script>
 
 <style scoped>
+a {
+  color: #3B7FEB;
+}
+
+a.hashtag {
+  box-shadow: 0px 1px 2px rgba(0,0,0,0.2);
+  display: inline-block;
+  margin-right: 2px;
+  border-radius: 20px;
+  padding: 4px 10px;
+  margin-bottom: 5px;
+}
+
 .avatar {
   box-shadow: 0px 2px 3px rgba(0,0,0,0.1);
   height: 46px;
