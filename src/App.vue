16af1c25edcb705630c9ceaec7253c46705d8831
Alasdair Monk <alasdair.monk@gmail.com>
2016-03-10 12:08:15 +0000

    Add basic UI & mock endpoint

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,50 @@
 <template>
   <div id="app">
-    <hello></hello>
-    Clicked: {{ count }} times
-    <button @click="increment">+</button>
-    <button @click="decrement">-</button>
+    <spinner v-if="!hasData"></spinner>
+    <weather v-if="hasData" :weather="weather" :location="location"></weather>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello.vue'
-import * as actions from './actions'
-
+import weather from './components/weather'
+import spinner from './components/spinner'
 export default {
   components: {
-    Hello
+    weather,
+    spinner
   },
-  vuex: {
-    getters: {
-      count: state => state.count
+  data () {
+    return {
+      weather: '',
+      location: '',
+      hasData: false
+    }
+  },
+  methods: {
+    getLocation: function () {
+      if (navigator.geolocation) {
+        console.log(navigator.geolocation.getCurrentPosition(this.updateData))
+      }
     },
-    actions: actions
+    updateData: function (position) {
+      var latitude = position.coords.latitude
+      var longitude = position.coords.longitude
+      var endpoint = `http://www.mocky.io/v2/56e160e0260000e101eaa21c?latitude=${latitude}&longitude=${longitude}`
+
+      this.hasData = true
+
+      console.log(`${latitude}, ${longitude}`)
+
+      window.fetch(endpoint).then(function (response) {
+        return response.json()
+      }).then(function (json) {
+        this.weather = json.weather
+        this.location = json.location
+      }.bind(this))
+    }
+  },
+  ready: function () {
+    this.getLocation()
   }
 }
 </script>
---
+++ src/components/Weather.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>{{weather}}</h1>
+    <h2>{{location}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['weather', 'location']
+}
+</script>
